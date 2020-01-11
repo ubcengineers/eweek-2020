4a6856e579c76958bbef151075e629933eb2d6df
Quentin Golsteyn <qgolsteyn@gmail.com>
2020-01-10 17:43:37 -0800

    Update schedule

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -14,86 +14,99 @@ export default () => {
         <h2>Sunday</h2>
         <Event
           name="Opening ceremonies"
-          description="10 PM - 12 AM"
+          description="10 PM - 12 AM / ESC"
           scores={["-", "-", "-"]}
         />
         <h2>Monday</h2>
         <Event
           name="Red Eye Breakfast"
-          description="8 AM - 10 AM"
+          description="8 AM - 10 AM / ESC"
           scores={[]}
         />
         <Event
           name="Smash Tourney!"
-          description="12 PM - 2 PM"
+          description="12 PM - 2 PM / ESC"
           scores={["-", "-", "-"]}
         />
         <Event
           name="Trivia"
-          description="4 PM - 5 PM"
+          description="4 PM - 5 PM / KAIS 2020"
           scores={["-", "-", "-"]}
         />
         <Event
           name="fEUSball"
-          description="6 PM - 8 PM"
+          description="6 PM - 8 PM / ESC"
           scores={["-", "-", "-"]}
         />
         <h2>Tuesday</h2>
-        <Event name="PP Breakfast" description="8 AM - 10 AM" scores={[]} />
+        <Event
+          name="PP Breakfast"
+          description="8 AM - 10 AM / ESC"
+          scores={[]}
+        />
         <Event
           name="Chariot Racing"
-          description="1 PM - 4 PM"
+          description="1 PM - 4 PM  / Main Mall"
           scores={["-", "-", "-"]}
         />
         <Event
           name="Pong"
-          description="5:30 PM - 7:30 PM"
+          description="5:30 PM - 7:30 PM / ESC"
           scores={["-", "-", "-"]}
         />
-        <Event name="Pit Purge" description="8 PM - 12 AM" scores={[]} />
+        <Event
+          name="Pit Purge"
+          description="8 PM - 12 AM / The Pit"
+          scores={[]}
+        />
         <h2>Wednesday</h2>
         <Event
           name="Dodgeball"
-          description="10 AM - 12 PM"
+          description="10 AM - 12 PM / Great Hall"
           scores={["-", "-", "-"]}
         />
-        <Event name="AGM" description="2 PM - 3 PM" scores={[]} />
+        <Event name="AGM" description="2 PM - 3 PM / KAIS 2020" scores={[]} />
         <Event
           name="Movie Roast-A-Thon"
-          description="4 PM - 6:30 PM"
+          description="4 PM - 6:30 PM / SWNG 222"
           scores={["-", "-", "-"]}
         />
         <Event
           name="4 Legged Race"
-          description="8 PM - 10 PM"
+          description="8 PM - 10 PM / ESC"
           scores={["-", "-", "-"]}
         />
         <h2>Thursday</h2>
         <Event
           name="True Engineer"
-          description="11 AM - 1 PM"
+          description="11 AM - 1 PM / ESC"
           scores={["-", "-", "-"]}
         />
         <Event
           name="Old Red New Red"
-          description="6 PM - 9 PM"
+          description="6 PM - 9 PM / ESC"
           scores={["-", "-", "-"]}
         />
         <h2>Friday</h2>
         <Event
           name="Cooking with Beer"
-          description="11 AM - 1 PM"
+          description="11 AM - 1 PM / ESC"
           scores={["-", "-", "-"]}
         />
         <Event
           name="filmfEUSt"
-          description="3:30 PM - 6 PM"
+          description="3:30 PM - 6 PM / SWNG 221"
+          scores={["-", "-", "-"]}
+        />
+        <Event
+          name="Red Night"
+          description="8 PM - 2 AM / ESC"
           scores={["-", "-", "-"]}
         />
         <h2>Saturday</h2>
         <Event
           name="100th Engineer's Ball"
-          description="6 PM - 12 AM"
+          description="6 PM - 12 AM / Hellenic Community Centre"
           scores={[]}
         />
         <ButtonRow
